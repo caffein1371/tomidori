29cd6bf5460a31a313605327219a7b56c5a6aafa
cahein <incidents1371@gmail.com>
2022-08-31 23:07:02 +0900

    20220831

--- ABC183C.py
+++ ABC183C.py
@@ -3,11 +3,12 @@ import io
 import sys
 
 _INPUT = """\
-4 330
-0 1 10 100
-1 0 20 200
-10 20 0 300
-100 200 300 0
+5 5
+0 1 1 1 1
+1 0 1 1 1
+1 1 0 1 1
+1 1 1 0 1
+1 1 1 1 0
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
@@ -18,3 +19,15 @@ Tlist = []
 for i in range(N):
     T = list(map(int,input().split()))
     Tlist.append(T)
+#print(Tlist)
+ans = 0
+root = tuple()
+for v in itertools.permutations(range(1,N),N-1):
+    root=(0,)+v+(0,)
+    temp = 0
+    for i in range(N):
+        #print (Tlist[root[i]][root[i+1]])
+        temp+=Tlist[root[i]][root[i+1]]
+    if temp==K:
+        ans+=1
+print (ans)
