b25d53a81eb0d385ef71a005be3a19e3188d9702
cahein <incidents1371@gmail.com>
2022-09-04 13:54:05 +0900

    20220904

--- ABC177C.py
+++ ABC177C.py
@@ -3,18 +3,9 @@ import io
 import sys
 
 _INPUT = """\
-3
+2
 1 2 3
+aaa
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N = int(input())
-Alist = list(map(int,input().split()))
-temp = 10**9+7
-ans = 0
-sumans = sum(Alist)
-for i in range(N):
-    sumans-=Alist[i]
-    ans+=sumans*Alist[i]
-
-print (ans%temp)
