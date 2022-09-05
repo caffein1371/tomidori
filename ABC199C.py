292af9c1aa53ef627331575b921f10f8b75cc1fa
cahein <incidents1371@gmail.com>
2022-09-05 14:37:33 +0900

    20220905

--- ABC199C.py
+++ ABC199C.py
@@ -19,16 +19,37 @@ sys.stdin = io.StringIO(_INPUT)
 N=int(input())
 S=list(input())
 Q=int(input())
-#print (S)
+S.insert(0,"0")
+flag = True
+ans = []
 for i in range(Q):
     T,A,B = map(int,input().split())
     #print (S[N:2*N])
     if T==2:
-        #S[0:N+1],S[N:2*N] = S[N:2*N],S[0:N+1]
-        temp = S[0:N]
-        S[0:N] = S[N:2*N]
-        S[N:2*N] = temp
-
+        if flag:
+            flag =False
+        else:
+            flag =True
     elif T==1:
-        S[A-1], S[B-1] = S[B-1], S[A-1]
-print ("".join(S))
+        if flag:
+            S[A],S[B]=S[B],S[A]
+        #A,BがNより大きいか小さいかで，A,Bの移動距離を変える
+        else:
+            if A<=N:
+                A+=N
+            else:
+                A-=N
+            if B<=N:
+                B+=N
+            else:
+                B-=N
+            S[A],S[B]=S[B],S[A]
+
+left = S[1:N+1]
+right = S[N+1:2*N+1]
+    #Nを境に入れ替える
+if flag ==True:
+    ans = left+right
+else:
+    ans = right+left
+print ("".join(ans))
