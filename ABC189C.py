dfb69ced496966268a6c619b6d2bfb2f30c8591d
cahein <incidents1371@gmail.com>
2022-09-09 16:59:13 +0900

    20220909

--- ABC189C.py
+++ ABC189C.py
@@ -3,9 +3,21 @@ import io
 import sys
 
 _INPUT = """\
-2
-1 2 3
-aaa
+6
+2 4 4 9 4 9
+
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
+import numpy as np
+N = int(input())
+Alist = list(map(int,input().split()))
+
+l,r,x = 0,0,0
+
+num = sorted(list(set(Alist)),reverse=True)
+
+temp = -10**15
+nlist = np.array(Alist)
+print (nlist)
+#for i in range(len(num)):
