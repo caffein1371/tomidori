961ab616ad2e43739102c9941cf375767fea9638
cahein <incidents1371@gmail.com>
2022-09-10 17:39:37 +0900

    20220910

--- ABC167C.py
+++ ABC167C.py
@@ -12,7 +12,35 @@ _INPUT = """\
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
 N,M,X = map(int,input().split())
-clist = []
+C = []
+A = []
 for i in range(N):
-    clist.append(list(map(int,input().split())))
-print (clist)
+    temp = list(map(int,input().split()))
+    C.append(temp[0])
+    A.append(temp[1:])
+#rint (C)
+#print (A)
+ansmin = 10**15
+
+#左にシフトさせる（１を立てる）
+#2^N試すことになる
+for i in range(1<<N):
+    cost = 0
+    skill = [0]*M
+    #パターンを試す
+    for shift in range(N):
+        #１つ右にシフトビットして，1とのandをとり１をとれば買ったとみなす
+        if i>>shift &1==1:
+            cost+=C[shift]
+            for j in range(M):
+                skill[j]+= A[shift][j]
+    if X<=min(skill):
+        ansmin = min(cost,ansmin)
+
+if ansmin ==10**15:
+    print (-1)
+else:
+    print (ansmin)
+
+
+
