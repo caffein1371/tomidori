d785379f8f94bef87c36dcfcdc09464731e3c100
cahein <incidents1371@gmail.com>
2022-09-20 08:53:01 +0900

    20220920

---
+++ ABC177D.py
@@ -0,0 +1,42 @@
+##########################################
+import io
+import sys
+
+_INPUT = """\
+2
+1 2 3
+aaa
+"""
+sys.stdin = io.StringIO(_INPUT)
+##########################################
+# UnionFindを用意
+class UnionFind:
+    def __init__(self,n):
+        self.n=n
+        self.parent_size=[-1]*n
+ 
+    def leader(self,a):
+        if self.parent_size[a]<0: return a
+        self.parent_size[a]=self.leader(self.parent_size[a])
+        return self.parent_size[a]
+ 
+    def merge(self,a,b):
+        x,y=self.leader(a),self.leader(b)
+        if x == y: return 
+        if abs(self.parent_size[x])<abs(self.parent_size[y]):x,y=y,x
+        self.parent_size[x] += self.parent_size[y]
+        self.parent_size[y]=x
+        return 
+ 
+    def same(self,a,b):
+        return self.leader(a) == self.leader(b)
+ 
+    def size(self,a):
+        return abs(self.parent_size[self.leader(a)])
+ 
+    def groups(self):
+        result=[[] for _ in range(self.n)]
+        for i in range(self.n):
+            result[self.leader(i)].append(i)
+        return [r for r in result if r!=[]]
+
