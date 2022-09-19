1a77d8850e3c385dfbfc77f9378b8d36273f49b2
cahein <incidents1371@gmail.com>
2022-09-19 17:14:13 +0900

    20220919

--- ABC190D.py
+++ ABC190D.py
@@ -8,3 +8,17 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
+N = int(input())
+
+#2N=x*yの問題に変換できる
+ans = 0
+i = 1
+while i*i<=2*N:
+    if 2*N%i==0:
+        x=i
+        y = 2*N//x
+        if y%2!=x%2:
+            ans+=1
+    i+=1
+
+print (ans*2)
