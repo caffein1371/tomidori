35eb766ef8ec3fc4ff88fb8ce0ae6a82e1f5d175
cahein <incidents1371@gmail.com>
2022-09-20 18:16:23 +0900

    20220920

--- ABC177D.py
+++ ABC177D.py
@@ -3,9 +3,11 @@ import io
 import sys
 
 _INPUT = """\
-2
-1 2 3
-aaa
+5 3
+1 2
+3 4
+5 1
+
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
@@ -40,3 +42,17 @@ class UnionFind:
             result[self.leader(i)].append(i)
         return [r for r in result if r!=[]]
 
+N,M = map(int,input().split())
+Uni = UnionFind(N)
+for i in range(M):
+    A,B = map(int,input().split())
+    A-=1
+    B-=1
+    Uni.merge(A,B)
+
+friendgroup = Uni.groups()
+friendsize = []
+for i in friendgroup:
+    friendsize.append(len(i))
+
+print (max(friendsize))
