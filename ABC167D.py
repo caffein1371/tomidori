e8a30b5d7e5dd054389ce05d245888db001ec44b
cahein <incidents1371@gmail.com>
2022-09-23 08:48:37 +0900

    20220923

---
+++ ABC167D.py
@@ -0,0 +1,13 @@
+##########################################
+import io
+import sys
+
+_INPUT = """\
+4 5
+3 2 4 1
+
+"""
+sys.stdin = io.StringIO(_INPUT)
+##########################################
+N,K = map(int,input().split())
+Alist =list(map(int,input().split()))
