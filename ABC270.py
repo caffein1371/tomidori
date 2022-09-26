250a3cd431a2a233ef83528ae52e0cb0e0ecd8e5
cahein <incidents1371@gmail.com>
2022-09-26 09:40:06 +0900

    20220926

--- ABC270.py
+++ ABC270.py
@@ -10,39 +10,13 @@ sys.stdin = io.StringIO(_INPUT)
 ##########################################
 X,Y,Z = map(int,input().split())
 
-if Y>X>0:
-    print (X)
-elif 0>X>Y:
+if abs(Y)>=abs(X):
     print (abs(X))
-elif X>Y>Z>0:
-    print (X)
-elif 0>Z>Y>X:
+elif X>Y>Z>0 or 0>Z>Y>X:
     print (abs(X))
-elif X>Y>0 and 0>Z:
-    print (X+abs(Z)*2)
-elif 0>Y>X and 0<Z:
-    print (abs(X)+Z*2)
-elif Z>X>Y>0:
+elif Y*Z<0 and (X>Y>0 or 0>Y>X):
+    print (abs(X)+abs(Z)*2)
+elif X*Y<0 :
+    print (abs(X))
+else:
     print (-1)
-elif X>Z>Y>0:
-    print (-1)
-elif 0>Y>X>Z:
-    print (-1)
-elif 0>Y>Z>X:
-    print (-1)
-elif X>Z>0 and 0>Y:
-    print (X)
-elif 0>Z>X and 0<Y:
-    print (abs(X))
-elif X>0 and 0>Y>Z:
-    print (X)
-elif 0>X and 0<Y<Z:
-    print (abs(X))
-elif X>0 and 0>Z>Y:
-    print (X)
-elif 0>X and 0<Z<Y:
-    print (abs(X)) 
-elif Z>X>0 and 0>Y:
-    print (X)
-elif 0>X>Z and 0<Y:
-    print (abs(X))
